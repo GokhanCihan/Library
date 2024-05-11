d76ceeb3d3e50747ccd3ef58a45b5612fcef6411
Gökhan Cihan <83141061+GokhanCihan@users.noreply.github.com>
2024-05-11 14:39:49 +0300

    handler for NotFoundException

---
+++ src/main/java/gokhan/cihan/Library/utilitiy/exceptionHandler/NotFoundException.java
@@ -0,0 +1,7 @@
+package gokhan.cihan.Library.utilitiy.exceptionHandler;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
